3c559b5b68a6e29b26753ebe79bbb54a48e5b94f
Peter T Cormack <petertcormack@gmail.com>
2014-03-29 18:20:43 -0700

    some cleanup

--- app/controllers/cases_controller.rb
+++ app/controllers/cases_controller.rb
@@ -1,29 +1,29 @@
 class CasesController < ApplicationController
   def index
     begin
-      @filter = ApplicationController.helpers.desk.filters.entries[0]
-      @cases = @filter.cases
+      @filter = get_filter
+      @cases = get_cases
     rescue DesApi::Error => e
       @errors = e.to_s
     end
   end
 
   def edit
-    @case = get_cases.entries.find(params[:id]).first
+    @case = get_a_case(params[:id])
     @case_model = Case.new({
       the_case: @case,
       labels: []
     })
-    @labels = ApplicationController.helpers.desk.labels.entries
+    @labels = get_labels
   end
 
   def update
-    @case = get_cases.entries.find(params[:id]).first
+    @case = get_a_case(params[:id])
     @case_model = Case.new({
       the_case: @case,
       labels: params[:case] ? params[:case][:labels] : []
     })
-    @labels = ApplicationController.helpers.desk.labels.entries
+    @labels = get_labels
 
     if @case_model.save
       redirect_to action: 'index'
@@ -34,10 +34,18 @@ class CasesController < ApplicationController
 
   private
   def get_filter
-    ApplicationController.helpers.desk.filters.entries[0]
+    ApplicationController.helpers.desk.filters.entries.first
   end
 
   def get_cases
     get_filter.cases
+  end
+
+  def get_a_case id = 3
+    ApplicationController.helpers.desk.filters.entries.first.cases.entries[1]
+  end
+
+  def get_labels
+    ApplicationController.helpers.desk.labels.entries
   end
 end
